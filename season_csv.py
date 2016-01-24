99521af276755ae752b7703edf329fe896e88fa5
ochiba0227 <newgroundofswim@gmail.com>
2016-01-24 17:41:03 +0900

    ユーザデータの作成

--- season_csv.py
+++ season_csv.py
@@ -5,6 +5,7 @@ import os
 import sys
 import multiprocessing
 import winsound
+import re
 from datetime import datetime
 
 ##ファイル名,データ配列,保存したいキー値
@@ -27,17 +28,12 @@ def decode_json(file):
     if os.path.getsize(file) != 0 and is_json(ext):
         with open(file, 'r') as f:
             json_dic = json.load(f)
-            effort_data = json_dic['segmentEffortData']
         ## ケイデンス等取得データは10種類ある。以下のように確認
     ##        print('CADENCE' in test[5].values())
     ##        距離は以下のように取得、単位はm
     ##        print(fenrifja_dic['segmentEffortData']['distance'])
             f.close()
-    ##    segmentidキーを追加
-        effort_data['segmentid']=effort_data['segment']['id']
-        effort_data['userid']=effort_data['athlete']['id']
-        return effort_data
-        
+            return json_dic        
 
 ##拡張子がjsonかどうか確認する
 def is_json(ext):
@@ -63,8 +59,64 @@ def get_dirs(path):
     if os.path.isdir(path):
         return path
 
+##辞書に鍵が存在するか確認
+def compare_dictkeys(key,data):
+    if key in data.keys():
+        return data[key]
+    return None
+
+##mappointsの最初と最後を取得
+def get_start_end_latlng(stream_data):
+    for data in stream_data:
+        if data['type'] == 'MAPPOINT':
+            points = data['mapPoints']
+            startlatlng = points[0]
+            endlatlng = points[len(points)-1]
+            return [startlatlng,endlatlng]
+    return None
+
+##平均、最高速度、最高速度を記録した地点をdictで取得
+def get_speeds(stream_data):
+    moving = []
+    speed = []
+    points = []
+    for data in stream_data:
+        if data['type'] == 'VELOCITY':
+            speed = data['data']
+        if data['type'] == 'MOVING':
+            moving = data['moving']
+        if data['type'] == 'MAPPOINT':
+            points = data['mapPoints']
+##    moving=falseでもvelocity=0ではない
+    counter = 0
+    speed_list = []
+    for m in moving:
+        if m is True:
+            speed_list.append(speed[counter])
+        counter += 1;
+
+    return_dict = {}
+    key = 0
+    return_dict['averageSpeed'] = sum(speed)/len(speed)
+    return_dict['maxSpeed'] = max(speed,key = speed.index)
+    return_dict['maxSpeedLat'] = points[key]['latitude']
+    return_dict['maxSpeedLng'] = points[key]['longitude']
+    if len(speed_list) > 0:
+        key_m = speed_list.index(max(speed_list))
+##        key_m = 0
+        return_dict['averageSpeedMoving'] = sum(speed_list)/len(speed_list)
+        return_dict['maxSpeedMoving'] = max(speed_list)
+        return_dict['maxSpeedLatMoving'] = points[key_m]['latitude']
+        return_dict['maxSpeedLngMoving'] = points[key_m]['longitude']
+    return return_dict
+
 ##取得したディレクトリからjsonファイルを全て読み込みcsv出力
 def get_files(path):
+##    fname = re.split( r'\\', path )
+##    if os.path.exists(".\\results\\season_"+fname[len(fname)-1]+".csv"):
+##        print("exists:"+str(fname))
+##        return
+
     files = os.listdir(path)
     data_list = []
     segment_id = ""
@@ -73,13 +125,49 @@ def get_files(path):
         if is_json(ext):
             data = decode_json(os.path.join(path,file))
             if data:
-                temp = {'month':data['startDateLocal']['date']['month'],
-                        'hour':data['startDateLocal']['time']['hour'],
-                        'userid':data['userid']}
+                effort_data = data['segmentEffortData']
+                stream_data = data['segmentStreamData']
+                effort_data['segmentid']=effort_data['segment']['id']
+                effort_data['userid']=effort_data['athlete']['id']
+                start_latlng,end_latlng = get_start_end_latlng(stream_data)
+                speeds_dict = get_speeds(stream_data)
+                temp = {'userid':effort_data['userid'],
+                        'startDate':effort_data['startDate'],
+                        'elapsedTime':effort_data['elapsedTime'],
+                        'year':effort_data['startDateLocal']['date']['year'],
+                        'month':effort_data['startDateLocal']['date']['month'],
+                        'day':effort_data['startDateLocal']['date']['day'],
+                        'hour':effort_data['startDateLocal']['time']['hour'],
+                        'minute':effort_data['startDateLocal']['time']['minute'],
+                        'distance':effort_data['distance'],
+                        'startIndex':effort_data['startIndex'],
+                        'endIndex':effort_data['endIndex'],
+                        'averageGrade':effort_data['segment']['averageGrade'],
+                        'maximumGrade':effort_data['segment']['maximumGrade'],
+                        'elevationHigh':effort_data['segment']['elevationHigh'],
+                        'elevationLow':effort_data['segment']['elevationLow'],
+                        'segmentstartLat':effort_data['segment']['startLatlng']['latitude'],
+                        'segmentstartlng':effort_data['segment']['startLatlng']['longitude'],
+                        'segmentendLat':effort_data['segment']['endLatlng']['latitude'],
+                        'segmentendlng':effort_data['segment']['endLatlng']['longitude'],
+                        'startLat':start_latlng['latitude'],
+                        'startLng':start_latlng['longitude'],
+                        'endLat':end_latlng['latitude'],
+                        'endLng':end_latlng['longitude']
+                        }
+                temp.update(speeds_dict)
+##                ケイデンスと心拍数は装置がある人のみ取得可能
+                keys = ['averageCadence','averageHeartrate','maxHeartrate','averageWatts']
+                for key in keys:
+                    temp[key] = compare_dictkeys(key,effort_data)
                 data_list.append(temp);
-                segment_id = data['segmentid']
-    keys = ['month','hour','userid']
-    write_csv(".\\results\\season_"+str(segment_id)+".csv",data_list,keys)
+                segment_id = effort_data['segmentid']
+##    csvに書き出す要素をkeyで指定
+##    keys = ['userid','month','hour']
+    if len(data_list) > 0:
+        keys = data_list[0].keys()
+        write_csv(".\\results\\season_"+str(segment_id)+".csv",data_list,keys)
+        print("finished:"+path)
     
 ##listからnoneを省く
 def remove_none(listdata):
@@ -112,7 +200,10 @@ if __name__ == '__main__':
     dirs = os.listdir(working_path)
     dirs = add_workingpath(dirs,working_path)
     p = multiprocessing.Pool()
+    p.daemon = True
     remove_none(p.map(get_files,dirs))
+##    for d in dirs:
+##        get_files(d)
     p.close()
     print("finished:"+datetime.now().strftime("%Y%m%d%H%M%S"))
     winsound.PlaySound('se_moa01.wav',winsound.SND_FILENAME)
